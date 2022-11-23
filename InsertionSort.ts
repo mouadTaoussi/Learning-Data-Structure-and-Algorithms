5f36ddcd4b143202e97a8f08b0cae9323e325a52
Mouad Taoussi <mouadtaoussi0@gmail.com>
2022-11-23 23:41:17 +0100

    learning bubble sort

---
+++ InsertionSort.ts
@@ -0,0 +1,17 @@
+let array_of_unsorted_unmbers: number[] = [ 2, 5, 3, 8, 6, 4, 1];
+
+
+function  insertionSort(arr: number[]): number[] {
+
+	for (var i = 0; i < length; ++i) {
+		// code...
+
+
+		while() {
+			
+		}
+	}
+
+	console.log(arr)
+	return arr;
+}
